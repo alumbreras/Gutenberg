e41e8804c74e6d01740724c1dabcb006e23a09f0
Alberto Lumbreras <alberto.lumbreras@gmail.com>
2017-02-28 15:41:43 +0100

    first test with some english books

--- sentences_length.R
+++ sentences_length.R
@@ -1,8 +1,15 @@
-
+# Measure the sentence lengths of many books in Gutenberg
+# Note: use nltk to get corpus from languages other than english
+# http://www.nltk.org/book/ch02.html
+#########################################################
 library(gutenbergr)
+library(data.table)
+library(ggplot2)
 
-df <- data.frame(title =  character(), author = character(),  language= character(), mean= numeric(), sd= numeric())
-
+# Download the data and create the dataframe with the counts
+############################################################
+df <- data.frame(matrix(nrow=0, ncol = 5))
+colnames(df) <- c("title", "author", "language", "mean", "sd")
 for (i in 1:100){
 	book <- gutenberg_download(i, meta_fields = c('title', 'author', 'language'))
 	text <- paste(tail(book$text,1000), collapse = " ")
@@ -10,8 +17,14 @@ for (i in 1:100){
 	lengths <- vapply(strsplit(sentences[[1]], "\\W+"), length, integer(1))
 	mean_length <- mean(lengths)
 	sd_length <- sd(lengths)
-	df$title <- book$title[1]
-	df$author <- book$author[1]
-	df$language <- book$language[1]
-	rbind(df, c(df$title, df$author, df$language, mean_length, sd_length))
+	df <- rbindlist(list(df, data.table(title = book$title[1], 
+	                        author = book$author[1], 
+	                        language = book$language[1], 
+	                        mean_length = mean_length, 
+	                        sd_length = sd_length)))
 }
+
+# Plot
+###########################################################
+df_filtered <- df %>% filter(mean<30)
+ggplot(df_filtered, aes(x = title, y = mean)) + geom_point() + theme_bw()
